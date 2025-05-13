a4ca53fe30331451e57184a4df006b112ae435ac
renardeinside <polarpersonal@gmail.com>
2025-05-13 19:08:25 +0200

    switch to stremeable transport

--- src/databricks/labs/mcp/servers/unity_catalog/app.py
+++ src/databricks/labs/mcp/servers/unity_catalog/app.py
@@ -1,64 +1,34 @@
-from mcp.server import NotificationOptions, Server
+from mcp.server import Server
 from mcp.types import Tool as ToolSpec
-from mcp.server.sse import SseServerTransport
 import uvicorn
+from databricks.labs.mcp.base import get_serveable_app
 from databricks.labs.mcp.servers.unity_catalog.tools import (
     Content,
 )
-from starlette.applications import Starlette
-from starlette.routing import Mount, Route
 from databricks.labs.mcp.servers.unity_catalog.cli import get_settings
 
 from databricks.labs.mcp._version import __version__ as VERSION
 from databricks.labs.mcp.servers.unity_catalog.server import get_tools_dict
 
 
-server = Server(name="mcp-unitycatalog", version=VERSION)
+app = Server(name="mcp-unitycatalog", version=VERSION)
 tools_dict = get_tools_dict(settings=get_settings())
 
 
-@server.list_tools()
+@app.list_tools()
 async def list_tools() -> list[ToolSpec]:
     return [tool.tool_spec for tool in tools_dict.values()]
 
 
-@server.call_tool()
+@app.call_tool()
 async def call_tool(name: str, arguments: dict) -> list[Content]:
     tool = tools_dict[name]
     return tool.execute(**arguments)
 
 
-sse = SseServerTransport("/messages/")
-
-
-# Define handler functions
-async def handle_sse(request):
-    async with sse.connect_sse(
-        request.scope, request.receive, request._send
-    ) as streams:
-        await server.run(
-            streams[0],
-            streams[1],
-            server.create_initialization_options(
-                notification_options=NotificationOptions(
-                    resources_changed=True, tools_changed=True
-                )
-            ),
-        )
-
-
-# Create Starlette routes for SSE and message handling
-routes = [
-    Route("/sse", endpoint=handle_sse),
-    Mount("/messages/", app=sse.handle_post_message),
-]
-
-# Create and run Starlette app
-app = Starlette(routes=routes)
-
-
 def start_app():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    serveable = get_serveable_app(app)
+    uvicorn.run(serveable, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
